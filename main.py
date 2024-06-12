50d1bfb7aa4341f2abd47e91b4f04549d53be17c
stserk <stserkoniuk@exadel.com>
2024-06-12 08:19:33 +0300

    high score_basic

--- main.py
+++ main.py
@@ -35,13 +35,13 @@ while game_is_on:
         board.increase_score()
     # Detect collision with wall
     if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
-        game_is_on = False
-        board.game_over()
+        snake.reset()
+        board.reset()
     # Detect collision with tail
     for segment in snake.segments[1:]:
         if snake.head.distance(segment) < 10:
-            game_is_on = False
-            board.game_over()
+            snake.reset()
+            board.reset()
 
 screen.exitonclick()
 
--- snake.py
+++ snake.py
@@ -18,6 +18,13 @@ class Snake:
         for position in STARTING_POSITIONS:
             self.add_segment(position)
 
+    def reset(self):
+        for seg in self.segments:
+            seg.goto(1000, 1000)
+        self.segments.clear()
+        self.create_snake()
+        self.head = self.segments[0]
+
     def add_segment(self, position):
         new_segment = Turtle("snake.gif")
         new_segment.color("Orchid")
