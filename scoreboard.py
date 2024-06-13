dd5f601ccbd3375a4d84abc307e82e34dabe2d7a
stserk <stserkoniuk@exadel.com>
2024-06-13 07:46:17 +0300

    high score_record

--- scoreboard.py
+++ scoreboard.py
@@ -1,5 +1,6 @@
 from turtle import Turtle
 
+
 ALIGNMENT = "center"
 FONT = ("Courier", 16, "normal")
 
@@ -8,7 +9,8 @@ class ScoreBoard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
-        self.high_score = 0
+        with open("data.txt") as data:
+            self.high_score = int(data.read())
         self.penup()
         self.goto(0, 270)
         self.color("AntiqueWhite1")
@@ -26,5 +28,7 @@ class ScoreBoard(Turtle):
     def reset(self):
         if self.score > self.high_score:
             self.high_score = self.score
+            with open("data.txt", mode="w") as data:
+                data.write(f"{self.high_score}")
         self.score = 0
         self.update_score()
